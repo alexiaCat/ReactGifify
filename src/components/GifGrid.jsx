725d63f306c82c3d3bbf8038b9cb1795e858e45a
alexiaCat <alexia_12ceci@hotmail.com>
2023-09-02 01:41:13 -0400

    [add hook]

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,27 +1,16 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifCard } from './GifCard';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-
-    const [images, setImages] = useState([]);
-
-    const getImages = async () => {
-        const newImages = await getGifs(category);
-        setImages(newImages)
-    }
-
-    useEffect(() => {
-        getImages();
-    }, [category])
-
-
-
+    const { images, isLoading } = useFetchGifs(category);
 
     return (
         <>
             <div className="text-center">
-            <h3 className="text-white text-2xl md:text-3xl lg:text-4xl mt-4 bg-blue-600 rounded-md inline-block px-2">{category}</h3>
+                <h3 className="text-white text-2xl md:text-3xl lg:text-4xl mt-4 bg-blue-600 rounded-md inline-block px-2">{category}</h3>
+                {
+                    isLoading && (<h2>Cargando...</h2>)
+                }
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-10 w-[calc(100vw - 128px)]">
